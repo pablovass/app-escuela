5831e2a64164c04a4fc3a29454fe4aa4ab106c05
pablo vass <faktory.fm@live.com>
2021-01-05 12:02:50 -0300

    Optimisando el programa.cs

--- EscuelaApp/Program.cs
+++ EscuelaApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using EscuelaApp.Entidades;
+using static System.Console; // me va omitir la palabra console
 
 namespace EscuelaApp
 {
@@ -9,33 +10,39 @@ namespace EscuelaApp
         {
             //objeto de parametro obcionales 
             var escuela = new Escuela("SchoolPorDefecto", 2021, TiposEscuela.Secundaria, "Argentina", "BA");
-            
-            
+
+
             escuela.Cursos = new Curso[]{
                 new Curso(){Nombre = "101"},
                 new Curso(){Nombre = "201"},
                 new Curso(){Nombre = "301"}
             };
-            
-         InprimirCursosEscuela(escuela);
+
+            ImprimirCursosEscuela(escuela);
         }
 
-        private static void InprimirCursosEscuela(Escuela escuela)
+        private static void ImprimirCursosEscuela(Escuela escuela)
         {
-            Console.WriteLine("=========================");
-            Console.WriteLine("Cursos de la escuela");
-            Console.WriteLine("=========================");
-            foreach (var curso in escuela.Cursos)
+            WriteLine("=========================");
+            WriteLine("Cursos de la escuela");
+            WriteLine("=========================");
+
+            //evitamos crash por valores nulos
+            if (escuela?.Cursos != null)
             {
-                Console.WriteLine($"Nombre {curso.Nombre}, id {curso.UniqueId} ");
+                foreach (var curso in escuela.Cursos)
+                {
+                    WriteLine($"Nombre {curso.Nombre}, id {curso.UniqueId} ");
+                }
             }
+
         }
 
         private static void ImprimirCurso(Curso[] arregloCursos)
         {
             foreach (var curso in arregloCursos)
             {
-                Console.WriteLine($"Nombre {curso.Nombre}, id {curso.UniqueId} ");
+                WriteLine($"Nombre {curso.Nombre}, id {curso.UniqueId} ");
             }
         }
 
