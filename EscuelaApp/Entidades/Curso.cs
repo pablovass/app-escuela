ddbf2a49ab1fa0dce3a31386eacdcc4abbc9a6cc
pablo vass <faktory.fm@live.com>
2021-01-05 10:58:00 -0300

    creando cursos en la app

--- EscuelaApp/Entidades/Curso.cs
+++ EscuelaApp/Entidades/Curso.cs
@@ -1,10 +1,21 @@
+using System;
+
 namespace EscuelaApp.Entidades
 {
     public class Curso
     {
-        public string Nombre { get; set; }
-        public string UniqueId{ get; set; }
+        public string Nombre { get;  set; }  
+        public string UniqueId{ get;private set; }// el get es privado
 
         public string Jornada{ get; set; }
+/*
+        public Curso()
+        {
+            UniqueId = Guid.NewGuid().ToString(); // id aleatorio
+            
+        }*/
+        public Curso()=>UniqueId = Guid.NewGuid().ToString(); // id aleatorio
+            
+        
     }
 }
--- EscuelaApp/Program.cs
+++ EscuelaApp/Program.cs
@@ -11,6 +11,24 @@ namespace EscuelaApp
             var esc4  = new Escuela("SchoolPorDefecto",2021,TiposEscuela.Secundaria,"Argentina","BA");
             Console.WriteLine(esc4);
             
+            var curso1 = new Curso (){
+                Nombre ="101"
+
+            };
+            var curso2 = new Curso (){
+                Nombre ="201"
+
+            }; 
+            var curso3 = new Curso (){
+                Nombre ="301"
+
+            }; 
+            Console.WriteLine(esc4);
+            System.Console.WriteLine("====================");
+
+            Console.WriteLine(curso1.Nombre+" , "+ curso1.UniqueId);
+            Console.WriteLine($"{curso2.Nombre} , {curso2.UniqueId}");
+            Console.WriteLine($"{curso3.Nombre} , {curso3.UniqueId}");
 
         }
     }
