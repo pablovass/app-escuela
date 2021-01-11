cfabdf02e1b3be23ac5032109065be51d5930440
Pablo Vallejos <faktory.fm@live.com>
2021-01-11 10:28:16 -0300

    creando interface Ilugar e implementandola

--- EscuelaAppPoo/Entidades/Escuela.cs
+++ EscuelaAppPoo/Entidades/Escuela.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
+using EscuelaAppPoo.Util;
 
 namespace EscuelaAppPoo.Entidades
 {
-   public class Escuela : ObjetoEscuelaBase
+   public class Escuela : ObjetoEscuelaBase, Ilugar
     {
         public int AñoDeCreacion { get; set; }
         public string Pais { get; set; }
         public string Ciudad { get; set; }
+
+        public string Direccion { get; set; }
         public TiposEscuela TipoEscuela{get;set;}
         public List<Curso> Cursos { get; set; }
        
@@ -35,6 +38,17 @@ namespace EscuelaAppPoo.Entidades
         public override string ToString()
         {
             return $" Nombre: {Nombre}, Tipo: {TipoEscuela}\n Pais: {Pais}, Cuidad: {Ciudad}";
+        }
+
+        public void LimpiarLugar()
+        {
+            Printer.DibujarLinea();
+            Console.WriteLine("Limpiando EScuela....");
+            foreach (var curso in Cursos)
+            {
+                curso.LimpiarLugar();
+            }
+            Console.WriteLine($"Escuela {Nombre} Limpio");
         }
     }
 }
