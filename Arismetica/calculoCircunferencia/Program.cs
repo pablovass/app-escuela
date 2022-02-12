0a6f03fb6ab2eafbe5a79dab227b333186374632
pablovass <faktory.fm@live.com>
2022-02-12 01:32:24 -0300

    update 2022

---
+++ Arismetica/calculoCircunferencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace calculoCircunferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI=3.14159;
+            double radio;
+
+
+            WriteLine("Escribe el valor del radio");
+            radio=Convert.ToDouble(Console.ReadLine());
+            double areaCirculo=PI*radio*radio;
+            WriteLine("el radio es {0}, y el area del circulo es {1}",radio,areaCirculo);
+        }
+    }
+}
