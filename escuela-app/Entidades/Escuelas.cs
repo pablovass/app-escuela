3eb9ae47eb72988bd696d43b7ac1e0c7ec1075de
Pablovass <faktory.fm@live.com>
2023-07-02 11:28:01 -0300

    Creación de la estructura básica de la escuela

--- escuela-app/Entidades/Escuelas.cs
+++ escuela-app/Entidades/Escuelas.cs
@@ -1,54 +1,26 @@
 using System;
 using System.Collections.Generic;
-using EscuelaAppPoo.Util;
 
-namespace EscuelaAppPoo.Entidades
+namespace escuela_app.Entidades
 {
-   public class Escuela : ObjetoEscuelaBase, Ilugar
+    public class Escuela
     {
-        public int AñoDeCreacion { get; set; }
-        public string Pais { get; set; }
-        public string Ciudad { get; set; }
-
-        public string Direccion { get; set; }
-        public TiposEscuela TipoEscuela{get;set;}
-        public List<Curso> Cursos { get; set; }
-       
-        // constructor
-        public Escuela(string nombre, int año)
-        {
-            Nombre = nombre;
-            AñoDeCreacion = año;
-        }
-        //constructor vacio
+        public string Nombre { get; set; }         // Propiedad para almacenar el nombre de la escuela
+        public int AñoDeCreacion { get; set; }     // Propiedad para almacenar el año de creación de la escuela
+        public string Pais { get; set; }           // Propiedad para almacenar el país de la escuela
+        public string Ciudad { get; set; }         // Propiedad para almacenar la ciudad de la escuela
+        public string Direccion { get; set; }      // Propiedad para almacenar la dirección de la escuela
+      
+        // Constructor por defecto
         public Escuela() { }
 
-        //constructor lamda
-        public Escuela(string nombre, string pais) => (Nombre, Pais) = (nombre, pais);
-        //constructor con un valor por defecto
-        public Escuela(string nombre, int año,TiposEscuela tipo, string pais="",
-                        string ciudad="")
-        {
-            (Nombre,AñoDeCreacion)=(nombre,año);
-            Pais=pais;
-            Ciudad=ciudad;
-        }
-
-        // metodo toString 
-        public override string ToString()
-        {
-            return $" Nombre: {Nombre}, Tipo: {TipoEscuela}\n Pais: {Pais}, Cuidad: {Ciudad}";
-        }
-
-        public void LimpiarLugar()
-        {
-            Printer.DibujarLinea();
-            Console.WriteLine("Limpiando EScuela....");
-            foreach (var curso in Cursos)
-            {
-                curso.LimpiarLugar();
-            }
-            Console.WriteLine($"Escuela {Nombre} Limpio");
-        }
+         // public Escuela(string nombre, int año)
+        // {
+        //    this.nombre = nombre;
+        //     this.AñoDeCreacion = año;
+        // }
+        
+        // Constructor con parámetros utilizando expresiones de asignación
+        public Escuela(string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre, año);
     }
 }
