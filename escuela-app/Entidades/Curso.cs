fdcfd5f465b0a906166abcfa739a3cb59e6e0879
Pablovass <faktory.fm@live.com>
2023-07-15 18:30:33 -0300

    feat: Crea un arreglo de objetos

--- escuela-app/Entidades/Curso.cs
+++ escuela-app/Entidades/Curso.cs
@@ -11,14 +11,10 @@ namespace escuela_app.Entidades
         public string UniqueId { get; private set; }
         public string Nombre { get; set; }
         public TiposJornada Jornada { get; set; }
-       // public Curso()
-       // {
-       //     UniqueId = Guid.NewGuid().ToString();
-       // }
-       //es lo mismo que lo comentado arriba
+      
+       //es lo mismo que un constructor construidos con llaves 
         public Curso()=> UniqueId = Guid.NewGuid().ToString();
         
-
     }
 
 }
--- escuela-app/Program.cs
+++ escuela-app/Program.cs
@@ -10,16 +10,17 @@ namespace escuela_app
             // Crear una instancia de la clase Escuela y asignar valores a sus propiedades
             var escuela = new Escuela("Vass School", 2023,TiposEscuela.Secundaria,
             ciudad:"Bogota",pais:"Argentina");
-            
-            var curso1=new Curso(){Nombre="101"};
-            var curso2=new Curso(){Nombre="102"};
-            var curso3=new Curso(){Nombre="103"};
+            var arreglegoCursos=new Curso[3];
+            arreglegoCursos[0]=new Curso {Nombre="101"};
+            arreglegoCursos[1]=new Curso {Nombre="102"};
+            arreglegoCursos[2]=new Curso {Nombre="103"};
+
             // Imprimir el nombre de la escuela en la consola
             Console.WriteLine(escuela);
             System.Console.WriteLine("=========="); //cw shortcut
-           Console.WriteLine(curso1.Nombre+" "+","+curso1.UniqueId);
-           Console.WriteLine($"{curso2.Nombre}, {curso2.UniqueId}");
-           Console.WriteLine(curso3.Nombre+" "+","+curso3.UniqueId);
+           Console.WriteLine(arreglegoCursos[0].Nombre+" "+","+arreglegoCursos[0].UniqueId);
+           Console.WriteLine($"{arreglegoCursos[1].Nombre}, {arreglegoCursos[1].UniqueId}");
+           Console.WriteLine(arreglegoCursos[2].Nombre+" "+","+arreglegoCursos[2].UniqueId);
         }
     }
 }
