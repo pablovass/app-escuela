15307cf9f0f157b20f3c60fdef4fc3e3a7e14404
Pablovass <faktory.fm@live.com>
2023-07-18 10:55:01 -0300

    fin de desarrollo por consola

--- escuela-app/Program.cs
+++ escuela-app/Program.cs
@@ -1,46 +1,86 @@
-﻿using Serilog;
+﻿using escuela_app.App;
 using escuela_app.Entidades;
-using escuela_app.App;
 using escuela_app.Util;
-using static System.Console;//con esta linea omitimos System.Console
+using static System.Console;
 
-
-namespace escuela_app
+namespace CoreEscuela
 {
     class Program
     {
         static void Main(string[] args)
         {
-            AppDomain.CurrentDomain.ProcessExit += AccionDelEvento; //evento 
+            AppDomain.CurrentDomain.ProcessExit += AccionDelEvento;
+            AppDomain.CurrentDomain.ProcessExit += (o, s) => Printer.Beep(2000, 1000, 1);
 
             var engine = new EscuelaEngine();
             engine.Inicializar();
             Printer.WriteTitle("BIENVENIDOS A LA ESCUELA");
+
             var reporteador = new Reporteador(engine.GetDiccionarioObjetos());
             var evalList = reporteador.GetListaEvaluaciones();
-            var listaAsignaturas = reporteador.GetListaAsignaturas();
-            var listaporAsignatura = reporteador.GetPromeAlumnPorAsignatura();
+            var listaAsg = reporteador.GetListaAsignaturas();
+            var listaEvalXAsig = reporteador.GetDicEvaluaXAsig();
+            var listaPromXAsig = reporteador.GetPromeAlumnPorAsignatura();
 
-            Dictionary<int, string> dicccionario = new Dictionary<int, string>();
+            Printer.WriteTitle("Captura de una Evaluación por Consola");
+            var newEval = new Evaluacion();
+            string nombre, notastring;
+            float nota;
 
-            dicccionario.Add(10, "JuanK");
+            WriteLine("Ingrese el nombre de la evaluación");
+            Printer.PresioneENTER();
+            nombre = Console.ReadLine();
 
-            dicccionario.Add(23, "Lorem Ipsum");
-
-            foreach (var keyValPair in dicccionario)
+            if (string.IsNullOrWhiteSpace(nombre))
             {
-                WriteLine($"Key: {keyValPair.Key} Valor: {keyValPair.Value}");
+                Printer.WriteTitle("El valor del nombre no puede ser vacio");
+                WriteLine("Saliendo del programa");
+            }
+            else
+            {
+                newEval.Nombre = nombre.ToLower();
+                WriteLine("El nombre de la evaluacion ha sido ingresado correctamente");
             }
 
-            var dictmp = engine.GetDiccionarioObjetos();
 
-            engine.ImprimirDiccionario(dictmp, true);
+            WriteLine("Ingrese la nota de la evaluación");
+            Printer.PresioneENTER();
+            notastring = Console.ReadLine();
+
+
+            try
+            {
+                newEval.Nota = float.Parse(notastring);
+                if (newEval.Nota < 0 || newEval.Nota > 5)
+                {
+                    throw new ArgumentOutOfRangeException("La nota debe estar entre 0 y 5");
+                }
+                WriteLine("La nota de la evaluacion ha sido ingresada correctamente");
+                return;
+            }
+            catch (ArgumentOutOfRangeException arge)
+            {
+                Printer.WriteTitle(arge.Message);
+                WriteLine("Saliendo del programa");
+            }
+            finally
+            {
+                Printer.WriteTitle("FINALLY");
+                Printer.Beep(2500, 500, 3);
+
+            }
+            // catch(Exception)
+            // {
+            //     Printer.WriteTitle("El valor de la nota no es un número válido");
+            //     WriteLine("Saliendo del programa");
+
 
         }
 
         private static void AccionDelEvento(object sender, EventArgs e)
         {
             Printer.WriteTitle("SALIENDO");
+            Printer.Beep(3000, 1000, 3);
             Printer.WriteTitle("SALIÓ");
         }
 
@@ -54,7 +94,7 @@ namespace escuela_app
             {
                 foreach (var curso in escuela.Cursos)
                 {
-                    WriteLine($"Nombre {curso.Nombre}, Id  {curso.UniqueId}");
+                    WriteLine($"Nombre {curso.Nombre  }, Id  {curso.UniqueId}");
                 }
             }
         }
